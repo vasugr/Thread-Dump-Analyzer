3b48359f9bf0b005038caee96807ee13e1a000b5
vasugr <vasug49@iitkgp.ac.in>
2021-07-06 15:22:59 +0530

    Removed redundant code

--- out/extension.js
+++ out/extension.js
@@ -14,11 +14,6 @@ const vscode = require("vscode");
 const vscode_1 = require("vscode");
 const controller_1 = require("./controller");
 const generateStringSummary_1 = require("./generateStringSummary");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is exec
-const toObject = (map = new Map) => Object.fromEntries(Array.from(map.entries(), ([k, v]) => v instanceof Map
-    ? [k, toObject(v)]
-    : [k, v]));
 function getTextEditor(document) {
     for (let te of vscode.window.visibleTextEditors) {
         if (te.document.fileName === document.fileName) {
@@ -31,8 +26,6 @@ function foldLines(document, foldLines) {
     return __awaiter(this, void 0, void 0, function* () {
         var str = "";
         foldLines.forEach(p => str += p + ",");
-        //console.log("folde error : ",foldLines);
-        //console.log("folding lines: " + str);
         const textEditor = getTextEditor(document);
         if (!textEditor) {
             return;
@@ -48,40 +41,6 @@ function foldLines(document, foldLines) {
     });
 }
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    //console.log('Congratulations, your extension "tda" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('tda.helloWorld', () => {
-    // 	// The code you place here will be executed every time your command is executed
-    // 	// Display a message box to the user
-    // 	//vscode.window.showInformationMessage('Hello World from Thread Dump Analyzer!');
-    // 	const editor = vscode.window.activeTextEditor;
-    // 	if (editor) {
-    // 		const document = editor.document;
-    // 		const text = document.getText();
-    // 		var summary:Map<string,Map<string,Array<ThreadInfo>>>=generateSummary(text);
-    // 		var o3 = toObject(summary);
-    // 		var jsonfile = JSON.stringify(o3);
-    // 		const panel = vscode.window.createWebviewPanel(
-    // 			'catCoding', // Identifies the type of the webview. Used internally
-    // 			'Thread Dump Summary', // Title of the panel displayed to the user
-    // 			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-    // 			{
-    // 				// Enable scripts in the webview
-    // 				enableScripts: true
-    // 			  } // Webview options. More on these later.
-    // 		);
-    // 		const onDiskPath = vscode.Uri.file( 
-    // 			path.join(context.extensionPath,'src', 'index.ts')
-    // 		 );
-    // 		 // And get the special URI to use with the webview
-    // 		 const tsURI = panel.webview.asWebviewUri(onDiskPath);
-    // 		panel.webview.html = getWebviewContent(tsURI,jsonfile);
-    // 	}
-    // });
     const disposable = vscode.commands.registerCommand('tda.helloWorld', function () {
         // Get the active text editor
         const editor = vscode.window.activeTextEditor;
@@ -97,15 +56,10 @@ function activate(context) {
         if (editor) {
             const document = editor.document;
             const selection = editor.selection.union(new vscode_1.Range(new vscode_1.Position(0, 0), new vscode_1.Position(document.lineCount, 0)));
-            // Get the word within the selection
             const text = document.getText(selection);
             var summary = controller_1.generateSummary(text);
-            // var o3 = toObject(summary);
-            // var jsonfile = JSON.stringify(o3,null,4);
             var foldlines = [];
             var ans = generateStringSummary_1.generateStringSummary(summary, foldlines);
-            //console.log("sumary generated ");
-            //fs.writeFileSync(filePath.toString(), ans.toString(), 'utf8');
             var setting = vscode.Uri.parse(filePath.fsPath);
             vscode.workspace.openTextDocument(setting).then((a) => {
                 vscode.window.showTextDocument(a, 1, false).then(e => {
@@ -118,31 +72,7 @@ function activate(context) {
                 console.error(error);
                 debugger;
             });
-            //console.log("\n\nfile saved\n ");			
-            // editor.edit(editBuilder => {
-            // 	editBuilder.replace(new Range(
-            // 		new Position(0, 0),
-            // 		new Position(document.lineCount, 0)), ans);
-            // });
-            // foldLines(editor.document,foldlines.reverse());
-            //console.log("foldedlines = ",foldlines);
         }
-        //console.log("\n\nokokok\n\n");
-        // vscode.workspace.openTextDocument(openPath).then(doc => {
-        // 	vscode.window.showTextDocument(doc);
-        // 	const editor1 = vscode.window.activeTextEditor;
-        // 	if(editor1){
-        // 		const document1 = editor1.document;
-        // 		console.log("new doc text = ",document1.getText());
-        // 		console.log("new doc path = ",document1.uri.path);
-        // 		editor1.edit(editBuilder => {
-        // 		editBuilder.replace(new Range(
-        // 				new Position(0, 0),
-        // 				new Position(document1.lineCount, 0)), ans);
-        // 		});
-        // 		foldLines(document1,foldlines.reverse());
-        // 	}
-        // });
     });
     context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,6 @@ import { Position, Range } from 'vscode';
 import { generateSummary } from './controller';
 import { generateStringSummary } from './generateStringSummary';
 import { ThreadInfo } from './model/ThreadInfo';
-import * as fs from 'fs';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is exec
-
-const toObject:any = (map = new Map) =>
-  Object.fromEntries
-    ( Array.from
-        ( map.entries()
-        , ([ k, v ]) =>
-            v instanceof Map
-              ? [ k, toObject (v) ]
-              : [ k, v ]
-        )
-    );
 
 function getTextEditor(document: vscode.TextDocument): vscode.TextEditor | null {
 
@@ -34,8 +19,6 @@ function getTextEditor(document: vscode.TextDocument): vscode.TextEditor | null 
 async function foldLines(document: vscode.TextDocument, foldLines: Array<number>) {
 	var str = "";
 	foldLines.forEach(p => str += p + ",");
-	//console.log("folde error : ",foldLines);
-	//console.log("folding lines: " + str);
 
 	const textEditor = getTextEditor(document);
 	if (!textEditor) { return; }
@@ -51,47 +34,6 @@ async function foldLines(document: vscode.TextDocument, foldLines: Array<number>
 }
 
 export function activate(context: vscode.ExtensionContext) {
-		// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	//console.log('Congratulations, your extension "tda" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('tda.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	//vscode.window.showInformationMessage('Hello World from Thread Dump Analyzer!');
-
-	// 	const editor = vscode.window.activeTextEditor;
-		
-	// 	if (editor) {
-	// 		const document = editor.document;
-	// 		const text = document.getText();
-
-	// 		var summary:Map<string,Map<string,Array<ThreadInfo>>>=generateSummary(text);
-	// 		var o3 = toObject(summary);
-	// 		var jsonfile = JSON.stringify(o3);
-	// 		const panel = vscode.window.createWebviewPanel(
-	// 			'catCoding', // Identifies the type of the webview. Used internally
-	// 			'Thread Dump Summary', // Title of the panel displayed to the user
-	// 			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-	// 			{
-	// 				// Enable scripts in the webview
-	// 				enableScripts: true
-	// 			  } // Webview options. More on these later.
-	// 		);
-	// 		const onDiskPath = vscode.Uri.file( 
-	// 			path.join(context.extensionPath,'src', 'index.ts')
-	// 		 );
-	// 		 // And get the special URI to use with the webview
-	// 		 const tsURI = panel.webview.asWebviewUri(onDiskPath);
-			 
-	// 		panel.webview.html = getWebviewContent(tsURI,jsonfile);
-			
-	// 	}
-
-	// });
 
 	const disposable = vscode.commands.registerCommand('tda.helloWorld', function () {
 		// Get the active text editor
@@ -114,17 +56,11 @@ export function activate(context: vscode.ExtensionContext) {
 				new Position(0, 0),
 				new Position(document.lineCount, 0)));
 
-			// Get the word within the selection
 			const text = document.getText(selection);
 			var summary:Map<string,Map<string,Array<ThreadInfo>>>=generateSummary(text);
 
-			// var o3 = toObject(summary);
-			
-	 		// var jsonfile = JSON.stringify(o3,null,4);
 			var foldlines:Array<number>=[];
 			var ans = generateStringSummary(summary,foldlines);
-			//console.log("sumary generated ");
-			//fs.writeFileSync(filePath.toString(), ans.toString(), 'utf8');
 			var setting: vscode.Uri = vscode.Uri.parse(filePath.fsPath);
 				vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
 					
@@ -140,37 +76,8 @@ export function activate(context: vscode.ExtensionContext) {
 				}, (error: any) => {
 					console.error(error);
 					debugger;
-				});
-			
-			//console.log("\n\nfile saved\n ");			
-			// editor.edit(editBuilder => {
-			// 	editBuilder.replace(new Range(
-			// 		new Position(0, 0),
-			// 		new Position(document.lineCount, 0)), ans);
-			// });
-
-			// foldLines(editor.document,foldlines.reverse());
-			//console.log("foldedlines = ",foldlines);
+				});	
 		}
-		//console.log("\n\nokokok\n\n");
-		// vscode.workspace.openTextDocument(openPath).then(doc => {
-		// 	vscode.window.showTextDocument(doc);
-		// 	const editor1 = vscode.window.activeTextEditor;
-		// 	if(editor1){
-		// 		const document1 = editor1.document;
-
-		// 		console.log("new doc text = ",document1.getText());
-		// 		console.log("new doc path = ",document1.uri.path);
-		// 		editor1.edit(editBuilder => {
-		// 		editBuilder.replace(new Range(
-		// 				new Position(0, 0),
-		// 				new Position(document1.lineCount, 0)), ans);
-		// 		});
-	
-		// 		foldLines(document1,foldlines.reverse());
-		// 	}
-		// });
-		
 	});
 
 	context.subscriptions.push(disposable);
